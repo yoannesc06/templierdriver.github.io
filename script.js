43f7483dc27a50164e855791ea351de732761ba2
yoannesc06 <yoannescobar67@gmail.com>
2025-05-13 09:26:46 +0200

    responsive finit

--- script.js
+++ script.js
@@ -6,3 +6,12 @@ burger.addEventListener("click", () => {
   nav.classList.toggle("active");
   burger.classList.toggle("open");
 });
+// INFO
+const h1 = document.querySelector(".scroll-fade");
+
+window.addEventListener("scroll", () => {
+  const rect = h1.getBoundingClientRect();
+  if (rect.top < window.innerHeight - 100) {
+    h1.classList.add("visible");
+  }
+});
