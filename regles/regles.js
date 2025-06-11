06a0b63ce83936713c8665c47d8b7eb2b42fc12c
yoannesc06 <yoannescobar67@gmail.com>
2025-06-11 10:54:25 +0200

    responsive

---
+++ regles/regles.js
@@ -0,0 +1,8 @@
+// MENU BURGER
+const burger = document.getElementById("burger");
+const nav = document.getElementById("nav-links");
+
+burger.addEventListener("click", () => {
+  nav.classList.toggle("active");
+  burger.classList.toggle("open");
+});
