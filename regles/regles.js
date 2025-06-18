476da5dea4c688ba64c37df73dfd1c6a3e80412a
yoannesc06 <yoannescobar67@gmail.com>
2025-06-18 10:00:05 +0200

    RGPD

--- regles/regles.js
+++ regles/regles.js
@@ -6,3 +6,10 @@ burger.addEventListener("click", () => {
   nav.classList.toggle("active");
   burger.classList.toggle("open");
 });
+// AVANCE SCROLL
+window.addEventListener("scroll", () => {
+  const scrollBar = document.querySelector(".scroll-progress-bar");
+  const totalHeight = document.body.scrollHeight - window.innerHeight;
+  const progress = (window.scrollY / totalHeight) * 100;
+  scrollBar.style.height = progress + "%";
+});
